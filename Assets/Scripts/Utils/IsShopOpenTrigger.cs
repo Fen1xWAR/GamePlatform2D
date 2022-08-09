e23ca35abc2028f27e3090b6ab5293600b9964f6
a1exfeb <alekskor1234@gmail.com>
2022-08-09 19:30:17 +0300

    vrotebalanimacii

---
+++ Assets/Scripts/Utils/IsShopOpenTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scripts
+{
+    public class IsShopOpenTrigger : MonoBehaviour
+    {
+        public Animator _animator;
+        public static readonly int IsShopOpen = Animator.StringToHash("is-opened");
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _animator.SetBool(IsShopOpen, true);
+        }
+
+        public void Disable()
+        {
+            _animator = GetComponent<Animator>();
+            _animator.SetBool(IsShopOpen, false);
+            SceneManager.UnloadSceneAsync("ShopMenu");
+        }
+    }
+}
